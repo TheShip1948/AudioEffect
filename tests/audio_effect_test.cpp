67b85ee4ddbf24564d25eeac00d1d5ed110fdb77
BeshoyKhalifa-aai <Beshoy.Khalifa@automotive-ai.com>
2021-03-09 15:50:16 +0100

    Add testcases to cover different scenarios of detect_feedback

--- tests/audio_effect_test.cpp
+++ tests/audio_effect_test.cpp
@@ -15,5 +15,54 @@ TEST_CASE("Feedback loop") {
 
         REQUIRE(detect_feedback(root) == false);
     }
+
+    SECTION("Single node with loop") {
+        root->next = root;
+
+        REQUIRE(detect_feedback(root) == true);
+    }
+
+    SECTION("Single node with no loop") {
+        REQUIRE(detect_feedback(root) == false);
+    }
+
+    SECTION("Multi node with loop on the tail node") {
+        std::shared_ptr<AudioEffect> first_node = std::make_shared<AudioEffectChild>();
+        std::shared_ptr<AudioEffect> second_node = std::make_shared<AudioEffectChild>();
+        std::shared_ptr<AudioEffect> third_node = std::make_shared<AudioEffectChild>();
+
+        root->next = first_node;
+        first_node->next = second_node;
+        second_node->next = third_node;
+        third_node->next = third_node;
+
+        REQUIRE(detect_feedback(root) == true);
+    }
+
+    SECTION("Multi node with loop, tail is connected to root") {
+        std::shared_ptr<AudioEffect> first_node = std::make_shared<AudioEffectChild>();
+        std::shared_ptr<AudioEffect> second_node = std::make_shared<AudioEffectChild>();
+        std::shared_ptr<AudioEffect> third_node = std::make_shared<AudioEffectChild>();
+
+        root->next = first_node;
+        first_node->next = second_node;
+        second_node->next = third_node;
+        third_node->next = root;
+
+        REQUIRE(detect_feedback(root) == true);
+    }
+
+    SECTION("Multi node with loop, tail is connected to middle node") {
+        std::shared_ptr<AudioEffect> first_node = std::make_shared<AudioEffectChild>();
+        std::shared_ptr<AudioEffect> second_node = std::make_shared<AudioEffectChild>();
+        std::shared_ptr<AudioEffect> third_node = std::make_shared<AudioEffectChild>();
+
+        root->next = first_node;
+        first_node->next = second_node;
+        second_node->next = third_node;
+        third_node->next = first_node;
+
+        REQUIRE(detect_feedback(root) == true);
+    }
 }
 
